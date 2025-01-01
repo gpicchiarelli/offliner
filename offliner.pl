42c5c47568300588140ade4c459e5b5a838ad29b
Giacomo Picchiarelli <gpicchiarelli@users.noreply.github.com>
2025-01-01 10:28:35 +0100

    Update offliner.pl
    Scaricamento di tutte le risorse, non solo di alcuni elementi

--- offliner.pl
+++ offliner.pl
@@ -199,13 +199,27 @@ sub download_page {
         return;
     }
 
-    # Estrazione dei link
     my $parser = HTML::LinkExtor->new(sub {
         my ($tag, %attr) = @_;
-        return unless $tag eq 'a' || $tag eq 'img' || $tag eq 'link' || $tag eq 'script';
-        if (my $link = $attr{href} || $attr{src}) {
+        
+        # Lista di tag da controllare
+        return unless $tag =~ /^(a|img|link|script|iframe|video|audio|source|object|embed|meta|track|form)$/;
+
+        # Estrazione link dai vari attributi
+        my $link = $attr{href} || $attr{src} || $attr{data} || $attr{action} || $attr{poster} || ($tag eq 'meta' ? $attr{content} : undef);
+
+        if ($link) {
+            # Gestione di meta-refresh
+            if ($tag eq 'meta' && $link =~ /URL=([^;]+)/i) {
+                $link = $1;
+            }
+
             my $abs_link = URI->new_abs($link, $url)->as_string;
-            $queue->enqueue([$abs_link, $depth + 1]) if $abs_link =~ /^https?:\/\//;
+
+            # Aggiungi alla coda solo link validi HTTP o HTTPS
+            if ($abs_link =~ /^https?:\/\//) {
+                $queue->enqueue([$abs_link, $depth + 1]);
+            }
         }
     });
     $parser->parse($content);
